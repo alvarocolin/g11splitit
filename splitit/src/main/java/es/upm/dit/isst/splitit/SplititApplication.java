a1d118f824e2863e92bb9b8adc0bc377301ec2ea
Alvaro <a.colin@alumnos.upm.es>
2025-03-28 22:25:54 +0100

    Version nuevo proyecto vacia

---
+++ splitit/src/main/java/es/upm/dit/isst/splitit/SplititApplication.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.splitit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplititApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplititApplication.class, args);
+	}
+
+}
---
+++ splitit/src/test/java/es/upm/dit/isst/splitit/SplititApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.splitit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplititApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
